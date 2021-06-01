5de060e37869b970a787bcf336984611bb1c60f7
Danny <ctct25@naver.com>
2021-06-01 17:56:09 +0900

    21/06/01 객체지향 수업내용

---
+++ src/com/javaex/ex00/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaex.ex00;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		String name = "니콘";
+		int price = 400000;
+		
+		System.out.println(name + ":" + price);
+		
+		String name2 = "LG그램";
+		int price2 = 1000000;
+		
+		System.out.println(name2 + ":" + price2);
+		*/
+		
+		
+	}
+
+}
